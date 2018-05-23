4c21b5b012d4d8c01c854eff7406c6c6e476210a
MysteryPancake <MysteryPancake@users.noreply.github.com>
2018-05-23 19:40:13 +1000

    removed text/css because it's not needed

--- scripts/Main.js
+++ scripts/Main.js
@@ -6,7 +6,6 @@ chrome.storage.sync.get({
 }, function(items) {
 	if (items.enabled) {
 		var css = document.createElement("style");
-		css.type = "text/css";
 		css.innerHTML = "img { content: url(\"" + items.url + "\") !important; }";
 		document.body.appendChild(css);
 		var images = document.getElementsByTagName("img");
