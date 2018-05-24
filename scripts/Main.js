8491ece82d954e70caf2ed2b9ab5a7be23ecc212
MysteryPancake <MysteryPancake@users.noreply.github.com>
2018-05-24 11:53:04 +1000

    3 second refresh and short name

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,16 +1,24 @@
 "use strict";
 
+var url = "";
+
+function replace() {
+	var images = document.getElementsByTagName("img");
+	for (var i = 0; i < images.length; i++) {
+		images[i].src = url;
+	}
+}
+
 chrome.storage.sync.get({
 	enabled: false,
 	url: ""
 }, function(items) {
 	if (items.enabled) {
+		url = items.url;
 		var css = document.createElement("style");
-		css.innerHTML = "img { content: url(\"" + items.url + "\") !important; }";
+		css.innerHTML = "img { content: url(\"" + url + "\") !important; }";
 		document.body.appendChild(css);
-		var images = document.getElementsByTagName("img");
-		for (var i = 0; i < images.length; i++) {
-			images[i].src = items.url;
-		}
+		setInterval(replace, 3000);
+		replace();
 	}
 });
